1a8bd70a403b0ec9aaefcda1148c55c5334b93bb
Grzegorz Wilczura <grzegorz.wilczura@gmail.com>
2025-01-10 23:49:57 +0100

    conterparty feature and test component

--- demo-app/src/app/app.routes.ts
+++ demo-app/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './features/counterparty/test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'test', component: TestComponent }];
